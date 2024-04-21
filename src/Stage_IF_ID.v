e3f070d1d2ce8278768e73edf60ca53d32b5cd04
Chris <kuicheng404@gmail.com>
2024-04-21 16:54:28 +0800

    The Branch bug in IF_ID has fixed

--- src/Stage_IF_ID.v
+++ src/Stage_IF_ID.v
@@ -19,27 +19,40 @@ output	[31:0]	pc_o, inst_o;
 
 reg [31:0]	pc_o, inst_o;
 reg [11:0] 	pcIm_o;
+reg	flush_state;
 
 always@(posedge clk_i) begin
 	if(~start_i) begin
 		pc_o <= 32'b0;
 		inst_o <= 32'b0;
 		pcIm_o <= 12'b0;
+		flush_state <= 1'b0;
 	end
-	else if(flush_i) begin
-		pc_o <= pc_i;
-		inst_o <= 32'b0;
-		pcIm_o <= 12'b0;
+	else if(flush_i || flush_state) begin /* NEW: flush two cycle */
+		if(!flush_state) begin
+			pc_o <= pc_i;
+			inst_o <= 32'b0;
+			pcIm_o <= 12'b0;
+			flush_state <= 1'b1;
+		end
+		else begin
+			pc_o <= pc_i;
+			inst_o <= 32'b0;
+			pcIm_o <= 12'b0;
+			flush_state <= 1'b0;
+		end
 	end
 	else if(hazard_i) begin
 		pc_o <= pc_i;
 		inst_o <= inst_o;
 		pcIm_o <= pcIm_i;
+		flush_state <= 1'b0;
 	end
 	else begin
 		pc_o <= pc_i;
 		inst_o <= inst_i;
 		pcIm_o <= pcIm_i;
+		flush_state <= 1'b0;
 	end
 end
 
